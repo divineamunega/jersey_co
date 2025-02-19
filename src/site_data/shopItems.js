7dbc8bfb23d9fec6af1b2a1d7cf531a06e651345
divineamunega <divineamunega@gmail.com>
2025-02-19 11:54:28 +0100

    modified:   src/site_data/shopItems.js

--- src/site_data/shopItems.js
+++ src/site_data/shopItems.js
@@ -18,11 +18,11 @@ class ShopItem {
   }
 
   get discountedPrice() {
-    return `${this.currency}${(this.price * (1 - this._percentDiscount / 100)).toFixed(2)}`;
+    return `${this.currency}${(this.price * (1 - this._percentDiscount / 100)).toLocaleString("en-NG", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
   }
 
   get originalPrice() {
-    return `${this.currency}${this.price.toFixed(2)}`;
+    return `${this.currency}${this.price.toLocaleString("en-NG", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
   }
 
   get percentDiscount() {
@@ -34,65 +34,65 @@ const items = [
   new ShopItem(
     img1,
     "Classic Barcelona Jersey",
-    89.99,
-    15,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/assets/shop/image1.png",
   ),
   new ShopItem(
     img2,
     "Brazil 2022 World Cup Jersey",
-    74.99,
-    25,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/assets/shop/image2.png",
   ),
   new ShopItem(
     img3,
     "Man. United 23/24 Away Jersey",
-    69.99,
+    7999.99,
     0,
-    "£",
+    "₦",
     "/assets/shop/image3.png",
   ),
   new ShopItem(
     img4,
     "Arsenal 23/24 Away Jersey",
-    79.99,
-    20,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/assets/shop/image4.png",
   ),
   new ShopItem(
     img5,
     "Spurs Limited Edition",
-    89.99,
-    15,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/assets/shop/image5.png",
   ),
   new ShopItem(
     img6,
     "Chelsea Limited Edition",
-    89.99,
-    15,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/assets/shop/image6.png",
   ),
   new ShopItem(
     img7,
     "Madrid Limited Edition",
-    89.99,
-    15,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/src/assets/shop/image7.png",
   ),
   new ShopItem(
     img8,
     "Arsenal Limited Edition",
-    89.99,
-    15,
-    "£",
+    7999.99,
+    0,
+    "₦",
     "/assets/shop/image8.png",
   ),
 ];
